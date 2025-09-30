641187c781c8ea7d211c466b2403f5ee79eee76f
russelake@gmail.com <russelake@gmail.com>
2025-09-30 20:23:38 +0800

    Add authentication features and DTOs for login
    - Updated `Auth.csproj` to reference new folder structure.
    - Implemented `AuthRepository` class with `LoginAsync` method.
    - Enhanced `IAuthRepository` interface with `LoginAsync` signature.
    - Introduced `LoginDTO` and `LoginCred` records for login data.

---
+++ BackendAPI/Modules/Auth/Data/Repository/AuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using Auth.DTO;
+
+namespace Auth.Data.Repository;
+
+public class AuthRepository : IAuthRepository
+{
+    public Task<LoginDTO> LoginAsync(LoginCred cred)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BackendAPI/Modules/Auth/Data/Repository/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using Auth.DTO;
+
+namespace Auth.Data.Repository;
+
+public interface IAuthRepository
+{
+
+    Task<LoginDTO> LoginAsync(LoginCred cred);
+
+}
