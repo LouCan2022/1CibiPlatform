59f04eb6783df3da01cc831eab62cd60442aa317
russelake@gmail.com <russelake@gmail.com>
2025-10-05 19:05:50 +0800

    Refactor JWT authentication and enhance logging and Add HttpCookieOnly
    - Improved JWT authentication setup in ServiceConfiguration.cs with HTTP-only cookie handling.
    - Added `HttpCookieOnlyKey` to appsettings.Development.json for secure token storage.
    - Enhanced LoginHandler.cs with logging for tracking login attempts and failures.
    - Updated GlobalUsing.cs to include Microsoft.Extensions.Logging for logging functionality.
    - Refactored AuthServiceConfiguration.cs for consistency and improved method structure.

--- BackendAPI/Modules/Auth/ServiceConfig/AuthServiceConfiguration.cs
+++ BackendAPI/Modules/Auth/ServiceConfig/AuthServiceConfiguration.cs
@@ -1,55 +1,56 @@
 ﻿namespace Auth.ServiceConfig
 {
-    public static class AuthServiceConfiguration
-    {
-        private const string assemblyName = "APIs";
-        private const string connStringSegment = "OnePlatform_Connection";
+	public static class AuthServiceConfiguration
+	{
+		private const string assemblyName = "APIs";
+		private const string connStringSegment = "OnePlatform_Connection";
 
 
-        #region MediaTR Config
-        public static IServiceCollection AddAuthMediaTR(this IServiceCollection services, Assembly assembly)
-        {
-            // Add MediatR
-            services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssembly(assembly);
-                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-            });
-            services.AddValidatorsFromAssembly(assembly);
-            services.AddExceptionHandler<CustomExceptionHandler>();
-            return services;
-        }
-        #endregion
+		#region MediaTR Config
+		public static IServiceCollection AddAuthMediaTR(this IServiceCollection services, Assembly assembly)
+		{
+			// Add MediatR
+			services.AddMediatR(config =>
+			{
+				config.RegisterServicesFromAssembly(assembly);
+				config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+				config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+			});
+			services.AddValidatorsFromAssembly(assembly);
+			services.AddExceptionHandler<CustomExceptionHandler>();
+			return services;
+		}
+		#endregion
 
-        #region Services
-        public static IServiceCollection AddAuthServices(this IServiceCollection services)
-        {
-            services.AddTransient<AuthInitialData>();
-            services.AddTransient<IPasswordHasherService, PasswordHasherService>();
-            services.AddScoped<IJWTService, JWTService>();
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            return services;
-        }
+		#region Services
+		public static IServiceCollection AddAuthServices(this IServiceCollection services)
+		{
+			services.AddHttpContextAccessor();
+			services.AddTransient<AuthInitialData>();
+			services.AddTransient<IPasswordHasherService, PasswordHasherService>();
+			services.AddScoped<IJWTService, JWTService>();
+			services.AddScoped<IAuthRepository, AuthRepository>();
+			return services;
+		}
 
-        #endregion
+		#endregion
 
-        #region Db Config
+		#region Db Config
 
-        public static IServiceCollection AddAuthInfrastructure(
-            this IServiceCollection services,
-            IConfiguration configuration)
-        {
-            services.AddDbContext<AuthApplicationDbContext>(options =>
-            {
-                options.UseNpgsql(
-                    configuration.GetConnectionString(connStringSegment),
-                    npgsqlOptions => npgsqlOptions.MigrationsAssembly(assemblyName)
-                );
-            });
-            return services;
-        }
+		public static IServiceCollection AddAuthInfrastructure(
+			this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			services.AddDbContext<AuthApplicationDbContext>(options =>
+			{
+				options.UseNpgsql(
+					configuration.GetConnectionString(connStringSegment),
+					npgsqlOptions => npgsqlOptions.MigrationsAssembly(assemblyName)
+				);
+			});
+			return services;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
