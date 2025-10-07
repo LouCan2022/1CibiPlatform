5def36e09f8b95aee1a0c796984534ca9a056e0c
russelake@gmail.com <russelake@gmail.com>
2025-10-07 11:56:37 +0800

    Add AuthRefreshToken entity and migration
    Introduce AuthRefreshToken entity to manage refresh tokens for user sessions.
    Add DbSet<AuthRefreshToken> in AuthApplicationDbContext and update
    OnModelCreating method. Create migration for new table and
    configuration class. Update model snapshot to reflect changes.

---
+++ BackendAPI/API/APIs/Migrations/Auth/20251007035452_AddRefreshtokenTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace APIs.Migrations.Auth
+{
+    /// <inheritdoc />
+    public partial class AddRefreshtokenTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthRefreshToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TokenHash = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    RevokedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RevokedReason = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthRefreshToken", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthRefreshToken");
+        }
+    }
+}
