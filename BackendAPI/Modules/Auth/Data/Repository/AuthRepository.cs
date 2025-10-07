2bb87ecb832c1a201b09ba22aaab3ffa34e44c00
russelake@gmail.com <russelake@gmail.com>
2025-10-07 15:28:44 +0800

    Refactor AuthRepository and improve UI components
    - Added `SaveRefreshToken` method to `AuthRepository`.
    - Updated `IAuthRepository` to include the new method.
    - Modified cookie expiration in `LoginHandler` for configurability.
    - Enhanced NotFound page in `App.razor` with MudBlazor components.
    - Adjusted navigation links in `MainLayout.razor` for routing.
    - Updated `GetUserInfoIfAuthenticated` in `AuthService` to return a sample string.
    - Changed default font in `app.css` and refined focus styles for buttons and links.
    - Ensured consistent text alignment across UI elements.

--- BackendAPI/Modules/Auth/Data/Repository/AuthRepository.cs
+++ BackendAPI/Modules/Auth/Data/Repository/AuthRepository.cs
@@ -1,35 +1,41 @@
-﻿namespace Auth.Data.Repository;
+﻿
+namespace Auth.Data.Repository;
 
 public class AuthRepository : IAuthRepository
 {
-    private readonly AuthApplicationDbContext _dbcontext;
+	private readonly AuthApplicationDbContext _dbcontext;
 
-    public AuthRepository(AuthApplicationDbContext dbcontext)
-    {
-        this._dbcontext = dbcontext;
-    }
+	public AuthRepository(AuthApplicationDbContext dbcontext)
+	{
+		this._dbcontext = dbcontext;
+	}
 
-    public async Task<LoginDTO> LoginAsync(LoginCred cred)
-    {
-        var userData = await (from user in _dbcontext.AuthUsers
-                              join userRole in _dbcontext.AuthUserAppRoles
-                                    on user.Id equals userRole.UserId into userRolesGroup
-                              where user.Username == cred.Username && user.IsActive == true
-                              select new LoginDTO(
-                                  user.Id,
-                                  user.Username,
-                                  user.PasswordHash,
-                                  user.Email!,
-                                  user.FirstName!,
-                                  user.LastName!,
-                                  user.MiddleName,
-                                  userRolesGroup.Select(r => r.AppId.ToString()).ToList(),
-                                  userRolesGroup.Select(r => r.RoleId.ToString()).ToList()
-                              )
-                          ).AsNoTracking()
-                           .FirstOrDefaultAsync();
+	public async Task<LoginDTO> LoginAsync(LoginCred cred)
+	{
+		var userData = await (from user in _dbcontext.AuthUsers
+							  join userRole in _dbcontext.AuthUserAppRoles
+									on user.Id equals userRole.UserId into userRolesGroup
+							  where user.Username == cred.Username && user.IsActive == true
+							  select new LoginDTO(
+								  user.Id,
+								  user.Username,
+								  user.PasswordHash,
+								  user.Email!,
+								  user.FirstName!,
+								  user.LastName!,
+								  user.MiddleName,
+								  userRolesGroup.Select(r => r.AppId.ToString()).ToList(),
+								  userRolesGroup.Select(r => r.RoleId.ToString()).ToList()
+							  )
+						  ).AsNoTracking()
+						   .FirstOrDefaultAsync();
 
 
-        return userData!;
-    }
+		return userData!;
+	}
+
+	public Task<bool> SaveRefreshToken(Guid userId, string refreshToken, DateTime expiryDate)
+	{
+		throw new NotImplementedException();
+	}
 }
