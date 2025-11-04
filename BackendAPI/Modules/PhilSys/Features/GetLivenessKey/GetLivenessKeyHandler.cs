0660562b458d064bd0331ba077ce8bc593a0f9b1
Renzy Gutierrez <rcgutierrez@cibi.com.ph>
2025-11-04 18:18:30 +0800

    Hid the SDK Liveness Key in the PhilSys API Backend

---
+++ BackendAPI/Modules/PhilSys/Features/GetLivenessKey/GetLivenessKeyHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PhilSys.Features.GetLivenessKey;
+
+public record GetLivenessKeyCommand() : ICommand<GetLivenessKeyResult>;
+
+public record GetLivenessKeyResult(string LivenessKey);
+public class GetLivenessKeyHandler : ICommandHandler<GetLivenessKeyCommand, GetLivenessKeyResult>
+{
+	private readonly GetLivenessKeyService _getLivenessKeyService;
+
+	public GetLivenessKeyHandler(GetLivenessKeyService GetLivenessKeyService)
+	{
+		_getLivenessKeyService = GetLivenessKeyService;
+	}
+	public async Task<GetLivenessKeyResult> Handle(GetLivenessKeyCommand request, CancellationToken cancellationToken)
+	{
+		
+		var livenessKey = await _getLivenessKeyService.GetLivenessKey();
+		return new GetLivenessKeyResult(livenessKey);
+	}
+}
--- UI/FrontendWebassembly/wwwroot/js/philsys/livenessInterop.js
+++ UI/FrontendWebassembly/wwwroot/js/philsys/livenessInterop.js
@@ -1,11 +1,11 @@
-﻿window.startLivenessInterop = (HashToken, dotNetHelper) => {
+﻿window.startLivenessInterop = (HashToken, dotNetHelper, livenessKey) => {
     if (!window.eKYC) {
         console.error("❌ eKYC SDK not loaded!");
         alert("Liveness SDK not loaded properly. Check script source.");
         return;
     }
     window.eKYC().start({
-        pubKey: "eyJpdiI6Im9YTTRTTXpwbDF0ZlRvakFHRG1HTnc9PSIsInZhbHVlIjoiUlo3WFJmM1dZUEVSdmNNbDJrU3o2Zz09IiwibWFjIjoiZjJmNWQxN2M4ZjgxMDQ1NDE5MzYzNTU1ZWNiMzU0MDk3Y2ZkNjc5NDA1Y2VlOTViOTQ5NmJhMWIzN2NiMzIxZCIsInRhZyI6IiJ9"
+        pubKey: livenessKey
     }).then((data) => {
         if (data && data.result && data.result.session_id) {
             const sessionId = data.result.session_id;
