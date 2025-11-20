b7f9f42f4683c5ac8961fd49f02c922506f2bfb1
russelake@gmail.com <russelake@gmail.com>
2025-11-20 11:03:48 +0800

    Refactor LoginService and enhance integration tests
    Refactored `LoginService` to improve token generation and error handling:
    - Moved JWT token generation after validating user applications, roles, and submenus.
    - Added validation for `appId`, `subMenuId`, and `roleId` with `UnauthorizedAccessException` for missing data.
    - Simplified user full name construction logic.
    
    Enhanced refresh token handling:
    - Added refresh token generation and database persistence.
    
    Improved `BaseIntegrationTest`:
    - Replaced manual record deletion with a `TRUNCATE` statement for reliable database cleanup.
    - Added `DbContext` dependency for database operations.
    
    Enhanced `LoginIntegrationTests`:
    - Added test cases for successful login, missing application scenarios, and invalid credentials.
    - Introduced `SeedUserData` and `SeedUserOnlyData` methods for comprehensive test data setup.
    
    Improved test data seeding:
    - Included roles, submenus, applications, and user-role mappings.
    - Standardized user ID generation for consistency.

--- Test/Test/BackendAPI/Modules/Auth.IntegrationTests/LoginIntegrationTests.cs
+++ Test/Test/BackendAPI/Modules/Auth.IntegrationTests/LoginIntegrationTests.cs
@@ -53,6 +53,7 @@ public class LoginIntegrationTests : BaseIntegrationTest
 	}
 
 
+
 	[Fact]
 	public async Task LoginWeb_ShouldReturnSuccess_WhenCredentialsAreCorrect()
 	{
@@ -74,6 +75,22 @@ public class LoginIntegrationTests : BaseIntegrationTest
 		result.loginResponseWebDTO.expires_in.Should().BeGreaterThan(0);
 	}
 
+	[Fact]
+	public async Task LoginWeb_ShouldUnauthorizedAccessException_WhenUserHasNoDesignatedApplication()
+	{
+		// Arrange
+		await SeedUserOnlyData();
+
+		var command = new LoginWebCommand(new LoginWebCred("john@example.com", "p@ssw0rd!", false));
+
+		// Act
+		Func<Task> act = async () => { await _sender.Send(command); };
+
+		// Assert
+		await act.Should().ThrowAsync<UnauthorizedAccessException>().WithMessage("Your account has no assigned application. Please contact an administrator for assistance.");
+	}
+
+
 
 	[Fact]
 	public async Task LoginWeb_ShouldReturnNotFound_WhenCredentialsAreCorrect()
@@ -92,16 +109,113 @@ public class LoginIntegrationTests : BaseIntegrationTest
 
 	private async Task SeedUserData()
 	{
+		var userId = Guid.NewGuid();
+
 		var user = new Authusers
 		{
-			Id = Guid.NewGuid(),
+			Id = userId,
 			Email = "john@example.com",
 			PasswordHash = _passwordHasherService.HashPassword("p@ssw0rd!"),
 			FirstName = "Admin",
 			MiddleName = "Admin",
 			LastName = "Admin"
 		};
+
+		var userRole = new List<AuthRole>
+			{
+				new AuthRole
+				{
+					RoleName = "SuperAdmin",
+					Description = "Super Admin"
+				},
+				new AuthRole
+				{
+					RoleName = "Admin",
+					Description = "Administrator Role"
+				},
+				new AuthRole
+				{
+					RoleName = "User",
+					Description = "User Role"
+				}
+			};
+
+
+		var submenu = new List<AuthSubMenu>
+			{
+				new AuthSubMenu
+				{
+					SubMenuName = "CNX Dashboard",
+					Description = "List of Subjects"
+				},
+				new AuthSubMenu
+				{
+					SubMenuName = "IDV",
+					Description = "Philsys IDV"
+				}
+			};
+
+		var authapplication = new List<AuthApplication>
+			{
+				new AuthApplication
+				{
+					AppName = "CNX",
+					Description = "Concentrix API"
+				},
+				new AuthApplication
+				{
+					AppName = "Philsys",
+					Description = "IDV"
+				}
+			};
+
+		var authUserRole = new List<AuthUserAppRole>
+			{
+				new AuthUserAppRole
+				{
+					UserId = userId,
+					AppId = 1,
+					Submenu = 1,
+					RoleId = 1,
+					AssignedBy = userId
+				},
+				new AuthUserAppRole
+				{
+					UserId = userId,
+					AppId = 2,
+					Submenu= 2,
+					RoleId = 2,
+					AssignedBy = userId
+				}
+			};
+
 		_dbContext.AuthUsers.Add(user);
+		_dbContext.AuthApplications.AddRange(authapplication);
+		_dbContext.AuthRoles.AddRange(userRole);
+		_dbContext.AuthSubmenu.AddRange(submenu);
+		_dbContext.AuthUserAppRoles.AddRange(authUserRole);
+
+		await _dbContext.SaveChangesAsync();
+	}
+
+
+
+	private async Task SeedUserOnlyData()
+	{
+		var userId = Guid.NewGuid();
+
+		var user = new Authusers
+		{
+			Id = userId,
+			Email = "john@example.com",
+			PasswordHash = _passwordHasherService.HashPassword("p@ssw0rd!"),
+			FirstName = "Admin",
+			MiddleName = "Admin",
+			LastName = "Admin"
+		};
+
+		_dbContext.AuthUsers.Add(user);
+
 		await _dbContext.SaveChangesAsync();
 	}
 }
